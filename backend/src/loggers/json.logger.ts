36095db0417850cbee3f638a2eb6d33265658e5e
Mikhail Chernyshev <lishainik@gmail.com>
2026-05-05 17:31:26 +0300

    logger fix

--- backend/src/loggers/json.logger.ts
+++ backend/src/loggers/json.logger.ts
@@ -2,7 +2,7 @@ import { LoggerService, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class JsonLogger implements LoggerService {
-  formatMessage(level: string, message: any, ...optionalParams: any[]) {
+  formatMessage(level: string, message: unknown, ...optionalParams: unknown[]) {
     return JSON.stringify({
       level,
       message,
@@ -10,23 +10,23 @@ export class JsonLogger implements LoggerService {
       timestamp: new Date().toISOString(),
     });
   }
-  log(message: any, ...optionalParams: any[]) {
+  log(message: unknown, ...optionalParams: unknown[]) {
     console.log(this.formatMessage('log', message, ...optionalParams));
   }
 
-  error(message: any, ...optionalParams: any[]) {
+  error(message: unknown, ...optionalParams: unknown[]) {
     console.error(this.formatMessage('error', message, ...optionalParams));
   }
 
-  warn(message: any, ...optionalParams: any[]) {
+  warn(message: unknown, ...optionalParams: unknown[]) {
     console.warn(this.formatMessage('warn', message, ...optionalParams));
   }
 
-  debug(message: any, ...optionalParams: any[]) {
+  debug(message: unknown, ...optionalParams: unknown[]) {
     console.debug(this.formatMessage('debug', message, ...optionalParams));
   }
 
-  verbose(message: any, ...optionalParams: any[]) {
+  verbose(message: unknown, ...optionalParams: unknown[]) {
     console.log(this.formatMessage('verbose', message, ...optionalParams));
   }
 }
